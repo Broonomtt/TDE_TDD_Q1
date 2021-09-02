b15b898e1e2765bf39ad81198e2059b1b770660a
Bruno <bruno_motta@outlook.com>
2021-09-01 21:05:23 -0300

    Update test_tde.py

--- test_tde.py
+++ test_tde.py
@@ -1,6 +1,6 @@
 import pytest
-from calc import soma, num
+from calc import soma
 
-@pytest.mark.parametrize('a,b,resultado', [(0, 0, 0), (-2,-5,-7), (-2, 5,3), (7.5, 5.4,12.9), (-1.0, -2.0,-3.0), ('DOIS', 2, None)])
-def test_soma(a,b, resultado):
-    assert soma(a,b) == resultado
+@pytest.mark.parametrize('n1,n2,resultado', [(0, 0, 0), (-2,-5,-7), (-2, 5,3), (7.5, 5.4,12.9), (-1.0, -2.0,-3.0), ('DOIS', 2, None)])
+def test_soma(n1, n2, resultado):
+    assert soma(n1, n2) == resultado
