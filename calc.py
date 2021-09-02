62f10dc23bc865c7676f6bd565a418cc461bc7e2
Bruno <bruno_motta@outlook.com>
2021-09-01 22:48:30 -0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,12 +1,12 @@
 def soma(n1, n2):
   if num(n1) and num(n2):
-        return float(n1) + float(n2)
-    else:
-        return None
+    return float(n1) + float(n2)
+  else:
+    return None
     
 def num(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
+  try:
+    float(s)
+      return True
+  except ValueError:
+      return False
